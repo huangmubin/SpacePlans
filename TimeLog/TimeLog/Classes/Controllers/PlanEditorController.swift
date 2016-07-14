659e1a178c92c691a419a8096218828c0f050e4f
huangmubin <huangmubin@qq.com>
2016-07-14 17:48:21 +0800

    Update Plan Editor.

--- TimeLog/TimeLog/Classes/Controllers/PlanEditorController.swift
+++ TimeLog/TimeLog/Classes/Controllers/PlanEditorController.swift
@@ -10,19 +10,37 @@ import UIKit
 
 class PlanEditorController: UIViewController {
 
+    // MARK: - Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.layoutIfNeeded()
+        
+        deployBackView()
+        deployPicker()
     }
     
-    @IBAction func back() {
-        self.dismissViewControllerAnimated(true, completion: nil)
+    // MARK: - Back View
+    
+    @IBOutlet weak var backView: View!
+    
+    func deployBackView() {
+        let lines: [CGFloat] = [40,backView.bounds.height - 100,backView.bounds.height - 50]
+        for line in lines {
+            let layer = Drawer.line(backView.bounds, x1: 10, y1: line, x2: backView.bounds.width - 10, y2: line, w: 1, dashPhase: 10)
+            layer.strokeColor = AppTint.mainColor().CGColor
+            backView.layer.insertSublayer(layer, atIndex: 0)
+        }
     }
-
+    
+    // MARK: - Days
+    
+    @IBOutlet weak var dayPicker: PickerView!
+    @IBOutlet weak var timePicker: PickerView!
+    
+    func deployPicker() {
+        dayPicker.selectRow(9, inComponent: 0, animated: true)
+        timePicker.selectRow(9, inComponent: 0, animated: true)
+    }
+    
 }
