0f5baebb4f3b75ed09a6f5a4ea27fbf96eeb9830
huangmubin <huangmubin@qq.com>
2016-07-16 18:04:33 +0800

    update

--- TimeLog/TimeLog/Classes/Controllers/LogListController.swift
+++ TimeLog/TimeLog/Classes/Controllers/LogListController.swift
@@ -10,27 +10,19 @@ import UIKit
 
 class LogListController: UIViewController {
 
+    // MARK: Load
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    // MARK: - Views
     
-    @IBOutlet weak var label: UILabel!
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    // MARK: TableView
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
 }
--- TimeLog/TimeLog/Classes/Views/Interfaces/LogListCell.swift
+++ TimeLog/TimeLog/Classes/Views/Interfaces/LogListCell.swift
@@ -10,22 +10,75 @@ import UIKit
 
 class LogListCell: UITableViewCell {
 
-    @IBOutlet weak var note: UILabel!
-    @IBOutlet weak var start: UILabel!
-    @IBOutlet weak var end: UILabel!
-    @IBOutlet weak var duration: UILabel!
     
+    // MARK: - Values
     
+    var index: NSIndexPath = NSIndexPath(forRow: 0, inSection: 0)
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    // MARK: Layers
+    
+    var shadowLayer = CALayer()
+    var backViewMaskLayer = CALayer()
+    
+    // MARK: Views
+    
+    @IBOutlet weak var backView: UIView!
+    
+    @IBOutlet weak var rightView: UIView!
+    
+    @IBOutlet weak var deleteButton: Button!
+    
+    @IBOutlet weak var content: UIView!
+    @IBOutlet weak var contentCenterLayout: NSLayoutConstraint!
+    
+    @IBOutlet weak var noteLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startLabel: UILabel!
+
+    
+    // MAKR: - Methods
+    
+    var singleDeploy = true
+    func deploy() {
+        guard singleDeploy else { return }
+        singleDeploy = false
+        
+        self.layoutIfNeeded()
+        // shadowLayer
+        //shadowLayer.frame = CGRect(x: 8, y: 4, width: frame.width - 16, height: frame.height - 8)
+        shadowLayer.frame = backView.frame
+        shadowLayer.cornerRadius    = 4
+        shadowLayer.shadowOffset    = CGSizeZero
+        shadowLayer.shadowOpacity   = 0.5
+        shadowLayer.shadowRadius    = 2
+        shadowLayer.backgroundColor = AppTint.backColor().CGColor
+        layer.insertSublayer(shadowLayer, atIndex: 0)
+        
+        // backView
+        backViewMaskLayer.cornerRadius = 4
+        backViewMaskLayer.backgroundColor = UIColor.whiteColor().CGColor
+        backViewMaskLayer.frame  = backView.bounds
+        backView.backgroundColor = AppTint.backColor()
+        backView.layer.masksToBounds = true
+        backView.layer.mask = backViewMaskLayer
+        
+        // Button View
+        rightView.backgroundColor = AppTint.subColor().C
+        
+        // Content
+        content.backgroundColor = AppTint.backColor()
+        content.layer.cornerRadius = 4
+        contentCenterLayout.constant = 0
+        self.layoutIfNeeded()
+        
+        // Label
+        noteLabel.font  = AppTint.mainFont()
+        timeLabel.font = AppTint.noteFont()
+        startLabel.font   = AppTint.noteFont()
+        
+        noteLabel.textColor  = AppTint.fontColor().main
+        timeLabel.textColor = AppTint.fontColor().sub
+        startLabel.textColor   = AppTint.fontColor().sub
     }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }
